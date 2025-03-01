4ed15f3f92a8e1175fa90b9494543681e3495219
e <e@e.ee>
2025-03-01 18:10:46 +0200

    new version 250301

--- lib/my_globals.dart
+++ lib/my_globals.dart
@@ -34,7 +34,7 @@ String xvSettHome = '';
 String xvBakDir = '';
 bool xvBusiness = false;
 
-const String progVersion = '0.8.250229';
+const String progVersion = '0.8.250301';
 // const String progDate = '2025-02-19';
 const String progAuthor = 'Eugen';
 const String progEmail = 'xxxx@xxx.xx';
