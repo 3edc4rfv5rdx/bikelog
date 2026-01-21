eca18e8a9596809c860eaf2ac799216e38d039c3
e <e@e.ee>
2026-01-21 12:03:38 +0200

    Version bump to 0.9.260121+46: go to github

--- lib/globals.dart
+++ lib/globals.dart
@@ -43,7 +43,7 @@ String xvBakDir = '';
 bool xvBusiness = false;
 
 const String progVersion = '0.9.260121';
-const int buildNumber = 45;
+const int buildNumber = 46;
 // const String progDate = '2025-02-19';
 const String progAuthor = 'Eugen';
 const String progEmail = 'xxxx@xxx.xx';
