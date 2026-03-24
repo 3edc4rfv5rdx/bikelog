fc1c3ba20ccc3a89914ab272b00b77a454191721
e <e@e.ee>
2026-03-24 11:33:36 +0200

    Version bump to 0.9.260324+56: after audit

--- lib/globals.dart
+++ lib/globals.dart
@@ -43,8 +43,8 @@ String xvSettHome = '';
 String xvBakDir = '';
 bool xvBusiness = false;
 
-const String progVersion = '0.9.260312';
-const int buildNumber = 55;
+const String progVersion = '0.9.260324';
+const int buildNumber = 56;
 const String progAuthor = 'Eugen';
 const String progEmail = 'xxxx@xxx.xx';
 const String progSite = 'bikelogbook.od.ua';
