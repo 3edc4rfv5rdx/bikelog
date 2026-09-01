71464f1c43ad3581494667bbdf0af9e902622178
e <e@e.ee>
2026-09-01 14:45:31 +0300

    Version bump to 0.9.260901+70: after audit

--- lib/globals.dart
+++ lib/globals.dart
@@ -61,7 +61,7 @@ String bikePhotoPath(String? fileName) {
 }
 
 const String progVersion = '0.9.260901';
-const int buildNumber = 69;
+const int buildNumber = 70;
 const String progAuthor = 'Eugen';
 const String progEmail = '3edc4rfv5rdx@gmail.com';
 const String progSite = 'bikelogbook.od.ua';
